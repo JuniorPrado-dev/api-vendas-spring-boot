d62bffb8c157a0c4968d14ead148de9328f871c9
Junior Prado <juniorcvnnn@gmail.com>
2023-08-31 23:48:14 -0300

    add dependencies and plugins

---
+++ src/main/java/jrdev/VendasApplication.java
@@ -0,0 +1,10 @@
+package jrdev;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //notation que reconhece a classe com a classe inicial
+public class VendasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class,args);
+    }
+}
