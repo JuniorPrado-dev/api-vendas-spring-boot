a4e86992a559b0ac23faf86480626755e77ac322
Junior Prado <juniorcvnnn@gmail.com>
2023-09-03 17:52:42 -0300

    add notations @GetMapping and starter-web

--- src/main/java/jrdev/VendasApplication.java
+++ src/main/java/jrdev/VendasApplication.java
@@ -1,10 +1,23 @@
 package jrdev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication //notation que reconhece a classe com a classe inicial
+@RestController
 public class VendasApplication {
+    //mapeia requisições da URL
+    @GetMapping("/hello")
+    public String HelloWorld (){
+        return "Olá pessoal!";
+    }
+    @GetMapping("")
+    public String starter (){
+        return "API Rodando!!";
+    }
     public static void main(String[] args) {
+
         SpringApplication.run(VendasApplication.class,args);
     }
 }
